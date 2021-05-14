df89dbdffeac6105e2a309856dfb2e5f7e09c801
onurkat <5752017+onurkat@users.noreply.github.com>
2021-05-15 00:44:04 +0300

    created changelog.sh script

---
+++ changelog.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Author : Onur Kat
+# Contact info : github.com/onurkat
+
+echo "***** Changelog Generator *****"
+echo "Checking Git status..."
+
+CHECK_GIT=$(git status | grep "not a git repository")
+if [ "" = "$CHECK_GIT" ]; then
+echo "OK."
+else
+echo "Error! Couldn't find the git repository. Are you in the correct directory?"
+return
+fi
+echo "Executing Git fetch to get latest tags..."
+git fetch --prune origin +refs/tags/*:refs/tags/* > /dev/null 2>&1
+echo "OK."
+
+LATEST_TAG=$(git tag --sort=-version:refname | head -n 1)
+
+echo "Use latest tag for deployment or change?"
+echo " 1) Use latest $LATEST_TAG"
+echo " 2) Change"
+while : 
+do
+read selection
+  case $selection in
+	1)	TAG_SEL_FLAG=1
+		break
+		;;
+	2)	TAG_SEL_FLAG=2
+		break			
+		;;
+	*)
+		echo "[Select 1 or 2 only]"		
+		;;
+  esac
+done
+
+if [ "2" = "$TAG_SEL_FLAG" ]; then
+echo "Listing last 10 tags. Please select a deployment tag..."
+TAGS=$(git tag --sort=-version:refname | head -n 10)
+select LATEST_TAG in $TAGS
+do
+echo "Selected deployment tag: "$LATEST_TAG
+break
+done
+fi
+
+echo "Listing last 10 tags. Please select a base tag to get changelogs from -> to "$LATEST_TAG
+TAGS=$(git tag --sort=-version:refname | head -n 11 | grep -v $LATEST_TAG)
+select OLD_TAG in $TAGS
+do
+echo "Selected tag: "$OLD_TAG
+break
+done
+echo "Please select the environment..."
+select ENV in "DEV" "STAGE" "PROD"
+do
+echo "Selected environment: "$ENV
+break
+done
+
+echo ""
+echo "Generating changelog... $OLD_TAG -> $LATEST_TAG"
+echo "OK."
+echo ""
+echo ""
+
+echo "*$LATEST_TAG $ENV Deployment*"
+echo ""
+echo "*Changelog (from $OLD_TAG)*"
+
+git log --date=format:"%d %b %H:%M" --pretty="- %s (%aN - %ad)" $LATEST_TAG...$OLD_TAG | grep -v "Merge pull request" | grep -v "Merge branch "
+
+echo ""
+echo ""
